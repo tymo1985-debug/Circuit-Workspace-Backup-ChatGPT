a894b4f467a82f1afb45b535e30f7292a1199cfc
tymo1985-debug <tymo1985@googlemail.com>
2026-08-09 14:31:54 +0200

    Add files via upload

--- circuit-planner/sw.js
+++ circuit-planner/sw.js
@@ -8,7 +8,7 @@
 
 const CACHE_STATIC = 'syp-static';
 const CACHE_RUNTIME = 'syp-runtime';
-// Step 35: pre-init i18n uses ordinary defer scripts; synchronous XHR is gone.
+// Step 36: calendar topbar divider and responsive alignment polished.
 const APP_SHELL_URLS = [
   './',
   './?source=pwa',
